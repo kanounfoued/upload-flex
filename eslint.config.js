4ad204ba719107ec043b199a7882f1b8cc725829
kanounfoued <foued.kanoun@openinnovation.ai>
2025-01-02 21:38:01 +0400

    feat: integrate eslint, prettier and config typescript

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    // Note: there should be no other properties in this object
+    ignores: ['node_modules', 'coverage', 'public', 'dist', 'yarn-lock'],
+  },
+  eslintPluginPrettierRecommended,
+];
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,35 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import { setupCounter } from './counter';
+import './style.css';
+import typescriptLogo from './typescript.svg';
+import viteLogo from '/vite.svg';
+
+/**
+ *
+ * Create a solution that does not rely on any external libraries.
+ *
+ * Upload use cases:
+ * - upload one file smaller than the MAX_CHUNK_SIZE
+ * - upload multuple small files
+ * - upload large file
+ * - upload multiple large files.
+ *
+ *
+ *
+ * use cases
+ * what if the file has failed  to upload?
+ * what if the the chunk has failed to upload?
+ *
+ *
+ * what if the user cancel a chunk upload?
+ * what if the user cancel a file upload?
+ * what if the user has paused the uplaod of a file ?
+ * what if the user has paused the uplaod of a chunk ?
+ * what if the user has resume the paused file upload?
+ * what if the user has resume the paused chunk upload?
+ *
+ *
+ *
+ */
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
@@ -19,6 +47,6 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
       Click on the Vite and TypeScript logos to learn more
     </p>
   </div>
-`
+`;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+setupCounter(document.querySelector<HTMLButtonElement>('#counter')!);
